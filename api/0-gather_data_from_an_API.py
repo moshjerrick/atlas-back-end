9de3a75e27960088e29161325c3ed0523792b957
Josh Merrick <moshjerrick@gmail.com>
2024-07-11 17:51:18 +0000

    line cuts and blank lines

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,7 +9,10 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     # Get user information
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    
+    user_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    )
     if user_response.status_code != 200:
         print(f"User with ID {employee_id} not found.")
         return
@@ -18,7 +21,9 @@ def get_employee_todo_progress(employee_id):
     user_name = user_data.get('name')
 
     # Get user's TODO list
-    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    todos_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    )
     if todos_response.status_code != 200:
         print(f"Could not retrieve TODO list for user ID {employee_id}.")
         return
@@ -31,9 +36,11 @@ def get_employee_todo_progress(employee_id):
     number_of_completed_todos = len(completed_todos)
 
     # Display progress
-    print(f"Employee {user_name} is done with tasks({number_of_completed_todos}/{total_todos}):")
+    print(f"Employee {user_name} is done with tasks("
+          f"{number_of_completed_todos}/{total_todos}):")
     for todo in completed_todos:
         print(f"\t {todo['title']}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
