cddedd46482612cff8e83b1504435289ad5cb3ac
Josh Merrick <moshjerrick@gmail.com>
2024-07-12 16:47:17 +0000

    chore: Export employee todo progress to CSV file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
 Python script that, using this REST API, for a given employee
-ID, returns information about his/her TODO list progress.
+ID, returns information about his/her TODO list progress and exports it to a CSV file.
 """
 
-import csv
 import requests
+import csv
 import sys
 
-
-def get_employee_todo_progress(employee_id):
+def get_employee_todo_progress_and_export(employee_id):
     # Get user information
     user_response = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{employee_id}'
@@ -17,10 +16,10 @@ def get_employee_todo_progress(employee_id):
     if user_response.status_code != 200:
         print(f"User with ID {employee_id} not found.")
         return
-
+    
     user_data = user_response.json()
     user_name = user_data.get('name')
-
+    
     # Get user's TODO list
     todos_response = requests.get(
         f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
@@ -28,31 +27,24 @@ def get_employee_todo_progress(employee_id):
     if todos_response.status_code != 200:
         print(f"Could not retrieve TODO list for user ID {employee_id}.")
         return
-
+    
     todos = todos_response.json()
-
-    # Calculate progress
-    total_todos = len(todos)
-    completed_todos = [todo for todo in todos if todo['completed']]
-    number_of_completed_todos = len(completed_todos)
-
-    # Display progress
-    print(f"Employee {user_name} is done with tasks("
-          f"{number_of_completed_todos}/{total_todos}):")
-    for todo in completed_todos:
-        print(f"\t {todo['title']}")
-
-    USER_ID = f"{employee_id}.csv"
-    with open(USER_ID, mode='w', newline='') as file:
+    
+    # Prepare data for CSV export
+    data_for_csv = [
+        [employee_id, user_name, todo['completed'], todo['title']] for todo in todos
+    ]
+    
+    # Export data to CSV
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
-        for todo in todos:
-            writer.writerow([employee_id,
-                             user_name,
-                             todo['completed'],
-                             todo['title']])
+        writer.writerows(data_for_csv)  # Writing data rows
+
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py EMPLOYEE_ID")
         sys.exit(1)
+    
